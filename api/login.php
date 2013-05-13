694c6f5df20e080642ce70ae4f23132581d28115
matt <matt@aevv.net>
2013-05-13 16:25:19 +0100

    forgot to hash password before checking

--- api/login.php
+++ api/login.php
@@ -1,10 +1,16 @@
 <?php
-if (!is_dir("puushsettings"))
+session_start();
+if (isset($_SESSION['k']))
+{
+	echo '{"error":"already logged in"}';
+	exit();
+}
+if (!is_dir("../puushsettings"))
 {
 	echo "no settings dir found";
 	exit();
 }
-include("puushsettings/settings.php");
+include("../puushsettings/settings.php");
 $con = mysql_connect($pHost . ":" . $pPort, $pUser, $pPass);
 if (!$con)
 {
@@ -12,15 +18,20 @@ if (!$con)
 }
 mysql_select_db($pDatabase, $con);
 
-$user = mysql_real_escape_string($_POST['e']);
-$pw = mysql_real_escape_string($_POST['p']);
+if (!isset($_GET['e']) || !isset($_GET['p']))
+{
+	echo '{"error":"bad parameters"}';
+	exit();
+}
+$user = mysql_real_escape_string($_GET['e']);
+$pw = mysql_real_escape_string($_GET['p']);
+$pw = md5(md5($pw));
 $ut = $pPrefix . "user";
 $res = mysql_query("SELECT * FROM $ut where email = '$user' and passwd = '$pw'");
 if (mysql_num_rows($res) == 1)
 {
 	while ($row = mysql_fetch_array($res))
 	{
-		session_start();
 		$_SESSION['k'] = $row['api_key'];
 		$_SESSION['e'] = $row['email'];
 		echo '{"success":"log in"}';
